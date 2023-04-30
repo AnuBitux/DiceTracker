b0809f66a377f528a2539e940b1e85322c3d17c1
AnuBitux <122746698+AnuBitux@users.noreply.github.com>
2023-04-30 16:42:07 +0200

    Create dicetracker.sh

---
+++ dicetracker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo 'DiceTracker is a tool that helps users to generate a private key tracking dice rolls'
+echo 'It allows to insert the result of every roll or to paste the result of previously made rolls'
+echo 'To use it simply run dicetracker.py'
+echo 'CAUTION: preferably run it offline!'
+echo ' '
+$SHELL
