59f22aad4dd74d95aeb117c6d31d9f5a98807b1b
AnuBitux <122746698+AnuBitux@users.noreply.github.com>
2024-03-24 22:35:45 +0100

    Update dicetracker.sh

--- dicetracker.sh
+++ dicetracker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+echo ' '
+figlet DiceTracker
+echo ' '
 echo 'DiceTracker is a tool that helps users to generate a private key tracking dice rolls'
 echo 'It allows to insert the result of every roll or to paste the result of previously made rolls'
 echo 'To use it simply run dicetracker.py'
